ee94a94a7ac68993a984e4fc6d1b6acf1f28ca79
Marcelo <marcelo@shipedge.com>
2020-11-22 17:27:38 -0400

    instalacion bootstrap

---
+++ resources/views/video/create.blade.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="/css/app.css" rel="stylesheet">
+    <title>Crear Video</title>
+</head>
+<body>
+    <div class="container">
+        <form action="" method="POST">
+            @csrf
+            <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                    <span class="input-group-text" id="basic-addon1">@</span>
+                </div>
+                <input type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+            </div>
+        </form>
+        <div class="d-flex align-items-center">
+            <strong>Subiendo video...</strong>
+            <div class="spinner-border ml-auto" role="status" aria-hidden="true"></div>
+        </div>
+    </div>
+</body>
+</html>
