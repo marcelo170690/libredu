3e667aeaf2b6772f32b561fc50ceab6315f7634d
jose luis <lcjl312.llampa@gmail.com>
2020-11-23 16:53:42 -0400

    terminando el form, botones y agregar archivos

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -19,13 +19,17 @@
                         <div class="input-group-prepend">
                             <span class="input-group-text" id="basic-addon1">Título</span>
                         </div>
-                        <input type="text" name="video" class="form-control" placeholder="Título">
+                        <input type="text" name="video" class="form-control" placeholder="Título" required>
                     </div>
+                    <div class="form-group">
+                        <label for="archivo"></label>
+                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
+                    </div>
+                    <button type="submit" class="btn btn-success btn-lg btn-block">Subir Video</button>
+                    <button type="button" class="btn btn-secondary btn-lg btn-block">Cancelar</button>
                 </form>
             </div>
             <div class="card-footer text-right">
-                <button type="button" class="btn btn-secondary">Cancelar</button>
-                <button type="button" class="btn btn-success">Subir Video</button>
             </div>
         </div>
     </div>
