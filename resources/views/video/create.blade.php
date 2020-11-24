98feb636049e5d6fefb55e22955c7a2bf69b6170
jose luis <lcjl312.llampa@gmail.com>
2020-11-24 16:21:02 -0400

    Arrastrar y soltar

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -4,6 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/app.css" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css">
+    <link rel="stylesheet" href="/css/dropzone.min.css">
+    <link rel="stylesheet" href="/css/estilos.css">
     <title>Crear Video</title>
 </head>
 <body>
@@ -13,25 +16,35 @@
                 Nuevo Video
             </div>
             <div class="card-body">
-                <form action="" method="POST">
-                    @csrf
-                    <div class="input-group">
-                        <div class="input-group-prepend">
-                            <span class="input-group-text" id="basic-addon1">Título</span>
+                <div class="form-container">
+                    <form class="dropzone" id="FormUploadFile" action="#" method="POST">
+                        <div class="dz-message">
+                            <div class="icon">
+                                <i class="fas fa-cloud-upload-alt"></i>
+                            </div>
+                            <h2>Arrastra tus videos aqui</h2>
                         </div>
+                    </form>
+                    
+                    <form action="" method="POST">
+                    <div class="input-group">
                         <input type="text" name="video" class="form-control" placeholder="Título" required>
+                    </div>
+                    <div class="input-group">
+                        <textarea class="form-control" id="exampleFormControlTextarea1" rows="3" placeholder="Descripcion"></textarea>
                     </div>
                     <div class="form-group">
                         <label for="archivo"></label>
-                        <input type="file" class="form-control-file" id="exampleFormControlFile1">
                     </div>
                     <button type="submit" class="btn btn-primary btn-lg btn-block">Subir Video</button>
                     <button type="button" class="btn btn-secondary btn-lg btn-block">Cancelar</button>
-                </form>
+                    </form>
+                </div>
             </div>
             <div class="card-footer text-right">
             </div>
         </div>
     </div>
+    <script src="/js/dropzone.js"></script>
 </body>
 </html>
