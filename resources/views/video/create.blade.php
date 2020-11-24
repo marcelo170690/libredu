ef34ef02303200b88a73a6b2033cbd302ca0f051
Marcelo <marcelo@shipedge.com>
2020-11-24 08:41:55 -0400

    add css

--- resources/views/video/create.blade.php
+++ resources/views/video/create.blade.php
@@ -4,6 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="/css/app.css" rel="stylesheet">
+    <link href="/css/global.css" rel="stylesheet">
     <title>Crear Video</title>
 </head>
 <body>
